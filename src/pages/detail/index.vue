8115bdd621b2e74125b5172890c1904dd094ebf8
阿宪 <ym18313983227@163.com>
2019-12-30 15:37:15 +0800

    创建文件

---
+++ src/pages/detail/index.vue
@@ -0,0 +1,28 @@
+<template>
+    <div class="list">
+        <div class="hero">
+            <img src="" alt="">
+            <p></p>
+        </div>
+    </div> 
+</template>
+
+<script>
+import heroList from '@/data/lol_duowan'
+export default {
+  data () {
+    return {
+      heroList
+    }
+  }
+
+}
+</script>
+
+<style lang="less">
+   .list{
+       display: flex;
+       flex-wrap: wrap;
+   }
+
+</style>
